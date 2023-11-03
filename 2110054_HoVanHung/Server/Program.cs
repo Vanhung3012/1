1f44c9e12232b45b958c5d89efe6a9d085067022
Hồ Văn Hưng <113756033+Vanhung3012@users.noreply.github.com>
2023-11-03 23:52:50 +0700

    Add files via upload

---
+++ 2110054_HoVanHung/Server/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using ThuVien;
+
+namespace Server
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            byte[] data = new byte[1024];
+            TcpListener server = new TcpListener(IPAddress.Any, 9050);
+            server.Start();
+            TcpClient client = server.AcceptTcpClient();
+            NetworkStream ns = client.GetStream();
+
+            while (true)
+            {
+                byte[] size = new byte[4];
+                int recv = ns.Read(size, 0, 4); 
+                int packsize = BitConverter.ToInt32(size, 0);// Nhận kích thước gói tin từ client gửi lên trước
+                if (packsize == 0) // Nếu như kích thước = 0 thì thoát
+                    break;
+                Console.WriteLine("Kich thuoc goi tin = {0}", packsize);
+                recv = ns.Read(data, 0, packsize); // Nhận mảng bytes chứa dữ liệu đối tượng sau
+                NhanVien emp1 = new NhanVien(data); // Chuyển bytes thành đối tượng
+                Console.WriteLine(emp1);
+            }
+            // Ngắt kết nối
+            ns.Close();
+            client.Close();
+            server.Stop();
+            Console.WriteLine("Ket thuc nhan du lieu");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 2110054_HoVanHung/ThuVien/TCPListener.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Text;
+
+namespace ThuVien
+{
+    public class TCPListener
+    {
+        public void Connect()
+        {
+            int recv;
+            byte[] data = new byte[1024];
+            TcpListener newsock = new TcpListener(9000);
+            newsock.Start();
+            Console.WriteLine("Waiting for a client...");
+            //
+            TcpClient client = newsock.AcceptTcpClient();
+            //
+            NetworkStream ns = client.GetStream();
+            Console.WriteLine(client.Client.RemoteEndPoint.ToString());
+            string welcome = "Welcome to server";
+            data = Encoding.ASCII.GetBytes(welcome);
+            ns.Write(data, 0, data.Length);
+            while (true)
+            {
+                data = new byte[1024];
+                recv = ns.Read(data, 0, data.Length);
+                if (recv == 0)
+                    break;
+                Console.WriteLine(Encoding.ASCII.GetString(data, 0, recv));
+                ns.Write(data, 0, recv);
+            }
+            ns.Close();
+            client.Close();
+            newsock.Stop();
+        }
+    }
+}
